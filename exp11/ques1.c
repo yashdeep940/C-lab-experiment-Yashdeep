9ae09356f047f96fe054c2e26ff930d0905224bb
yashdeep940 <Yashdeep.23466@stu.upes.ac.in>
2025-12-05 17:25:14 +0530

    Add files via upload

---
+++ exp11/ques1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 5;
+
+    printf("Bitwise OR of a and b is: %d\n", a | b);
+    printf("Bitwise AND of a and b is: %d\n", a & b);
+    printf("Bitwise NOT of a is: %d\n", ~a);
+
+    return 0;
+}
---
+++ exp11/ques2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    int a = 10;
+
+    printf("Left shift operator (a << 1): %d\n", a << 1);
+    printf("Right shift operator (a >> 1): %d\n", a >> 1);
+
+    return 0;
+}
